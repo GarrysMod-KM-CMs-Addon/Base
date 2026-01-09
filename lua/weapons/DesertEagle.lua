6c66a7c0632e1715f9fa85dcf1395343d6c3f8a7
KM_CM <send.nex@gmail.com>
2026-01-09 18:30:09 +0200

    And another!

--- lua/weapons/DesertEagle.lua
+++ lua/weapons/DesertEagle.lua
@@ -31,9 +31,6 @@ SWEP.flViewModelX = -8
 SWEP.flViewModelY = -2
 SWEP.flViewModelZ = 1
 
-SWEP.flBlindFireLeftX = 6
-SWEP.flBlindFireRightX = -16
-
 SWEP.vViewModelAim = Vector( -12 - SWEP.flViewModelX, -6.36 - SWEP.flViewModelY, 2.18 - SWEP.flViewModelZ )
 
 SWEP.__VIEWMODEL_FULLY_MODELED__ = true
