081140596ba7a7f87147d9946269ee97e32689f6
KM_CM <send.nex@gmail.com>
2026-01-09 18:29:15 +0200

    Another stupid fix. Jeez.

--- lua/weapons/UMP45.lua
+++ lua/weapons/UMP45.lua
@@ -24,7 +24,7 @@ SWEP.AdminOnly = false
 SWEP.Weight = 1
 SWEP.Slot = 2
 SWEP.DrawAmmo = true
-SWEP.vSprintArm = Vector( 1.358, -8.228, -.94 )
+SWEP.vSprintArm = Vector( -8.228, 1.358, -.94 )
 SWEP.Crosshair = "SubMachineGun"
 
 sound.Add {
