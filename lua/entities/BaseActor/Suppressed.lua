5b3e7933dd2dc6dacbd2b97a680446600a6254cb
KM_CM <send.nex@gmail.com>
2026-02-23 19:37:51 +0200

    Delete lua directory

--- lua/entities/BaseActor/Suppressed.lua
+++
@@ -1,6 +0,0 @@
-ENT.flShootTimeMin = 2
-ENT.flShootTimeMax = 12
-
-function ENT:CanExpose( MyTable, f ) return self.GAME_flSuppression <= self:Health() * self.flSuppressionHide end
-function ENT:IsSuppressed() return self.GAME_flSuppression > self:Health() * self.flSuppressionHide end
-function ENT:GetExposedWeight() return self.GAME_flSuppression end
