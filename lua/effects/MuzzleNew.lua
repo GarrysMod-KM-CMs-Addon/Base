4ddc3f90718296fdaafaeae1d058a1a176118b1e
KM_CM <send.nex@gmail.com>
2026-03-03 16:19:15 +0200

    /effects/

---
+++ lua/effects/MuzzleNew.lua
@@ -0,0 +1,39 @@
+function EFFECT:Init( Data )
+	self:SetRenderMode( RENDERMODE_TRANSALPHA )
+	local pWeapon = Data:GetEntity()
+	if !IsValid( pWeapon ) then return end
+	local pOwner = pWeapon:GetOwner()
+	if !IsValid( pOwner ) then return end
+	local v = self:GetTracerShootPos( Data:GetOrigin(), pWeapon, Data:GetAttachment() )
+	local vVelocity = pOwner:GetVelocity()
+	local pEmitter = ParticleEmitter( v )
+	for i = 1, 2 do
+		local pPart = pEmitter:Add( "effects/muzzleflash" .. math.random( 1, 4 ), v )
+		pPart:SetVelocity( vVelocity )
+		pPart:SetDieTime( .1 )
+		pPart:SetStartAlpha( 255 )
+		pPart:SetEndAlpha( math.Rand( 0, 255 ) )
+		pPart:SetStartSize( 1 * i )
+		pPart:SetEndSize( 7 * i )
+		pPart:SetRoll( math.Rand( 180, 480 ) )
+		pPart:SetRollDelta( math.Rand( -1, 1 ) )
+		pPart:SetColor( 255, 255, 255 )	
+		pPart:SetAirResistance( 140 )
+	end
+	for i = 1, 3 do 
+		local pPart = pEmitter:Add( "particle/particle_smokegrenade", v )
+		pPart:SetVelocity( vVelocity + Vector( math.Rand( -20, 20 ), math.Rand( -20, 20 ), math.Rand( -20, 20 ) ) )
+		pPart:SetDieTime( math.Rand( .5, 1 ) )
+		pPart:SetStartAlpha( math.Rand( 50, 155 ) )
+		pPart:SetStartSize( math.Rand( .5, 1 ) * i )
+		pPart:SetEndSize( math.Rand( 2, 3 ) * i )
+		pPart:SetRoll( math.Rand( 180, 480 ) )
+		pPart:SetRollDelta( math.Rand( -1, 1 ) )
+		pPart:SetColor( 200, 200, 200 )
+		pPart:SetAirResistance( 140 )
+	end
+	pEmitter:Finish()
+end
+
+function EFFECT:Think() return false end
+function EFFECT:Render() end
---
+++ lua/effects/MuzzleNewSilenced.lua
@@ -0,0 +1,26 @@
+function EFFECT:Init( Data )
+	self:SetRenderMode( RENDERMODE_TRANSALPHA )
+	local pWeapon = Data:GetEntity()
+	if !IsValid( pWeapon ) then return end
+	local pOwner = pWeapon:GetOwner()
+	if !IsValid( pOwner ) then return end
+	local v = self:GetTracerShootPos( Data:GetOrigin(), pWeapon, Data:GetAttachment() )
+	local vVelocity = pOwner:GetVelocity()
+	local pEmitter = ParticleEmitter( v )
+	for i = 1, 3 do
+		local pPart = pEmitter:Add( "particle/particle_smokegrenade", v )
+		pPart:SetVelocity( vVelocity + Vector( math.Rand( -20, 20 ), math.Rand( -20, 20 ), math.Rand( -20, 20 ) ) )
+		pPart:SetDieTime( math.Rand( .5, 1 ) )
+		pPart:SetStartAlpha( math.Rand( 50, 155 ) )
+		pPart:SetStartSize( math.Rand( .5, 1 ) * i )
+		pPart:SetEndSize( math.Rand( 2, 3 ) * i )
+		pPart:SetRoll( math.Rand( 180, 480 ) )
+		pPart:SetRollDelta( math.Rand( -1, 1 ) )
+		pPart:SetColor( 200, 200, 200 )
+		pPart:SetAirResistance( 140 )
+	end
+	pEmitter:Finish()
+end
+
+function EFFECT:Think() return false end
+function EFFECT:Render() end
