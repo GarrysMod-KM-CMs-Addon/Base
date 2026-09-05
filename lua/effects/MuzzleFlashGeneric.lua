1bdfa343e8f5cbd11738948986fb2ac2656b1eaa
KM_CM <197471590+KM-CM@users.noreply.github.com>
2026-09-05 14:22:01 +0300

    Add files via upload

--- lua/effects/MuzzleFlashGeneric.lua
+++ lua/effects/MuzzleFlashGeneric.lua
@@ -15,28 +15,30 @@ function EFFECT:Init( pData )
 	local fGetPos, fGetAngles = function() return vector_origin end, function() return angle_zero end
 
 	local v = pData:GetStart()
-	local pEntity, pOwner = pData:GetEntity()
+	local pEntity = pData:GetEntity()
+	local pOwner = pEntity:GetOwner()
 
 	local fGetMuzzleFlashPosition = pEntity.GetMuzzleFlashPosition
-	if fGetMuzzleFlashPosition then
+	local bCustomPosition = fGetMuzzleFlashPosition && fGetMuzzleFlashPosition( pEntity, "MuzzleFlashGeneric", iGetMaterialIndex )
+	if bCustomPosition then
 		local iGetMaterialIndex = pData:GetMaterialIndex()
 		fGetPos = function()
 			if !IsValid( pEntity ) then return vector_origin end
-			return fGetMuzzleFlashPosition( pEntity, "MuzzleFlashGeneric", iGetMaterialIndex )
+			return fGetMuzzleFlashPosition( pEntity, "MuzzleFlashGeneric", iGetMaterialIndex ) || vector_origin
 		end
 	end
 
 	local fGetMuzzleFlashAngles = pEntity.GetMuzzleFlashAngles
-	if fGetMuzzleFlashAngles then
+	local bCustomAngles = fGetMuzzleFlashAngles && fGetMuzzleFlashAngles( pEntity, "MuzzleFlashGeneric", iGetMaterialIndex )
+	if bCustomAngles then
 		local iGetMaterialIndex = pData:GetMaterialIndex()
 		fGetAngles = function()
 			if !IsValid( pEntity ) then return angle_zero end
-			return fGetMuzzleFlashAngles( pEntity, "MuzzleFlashGeneric", iGetMaterialIndex )
+			return fGetMuzzleFlashAngles( pEntity, "MuzzleFlashGeneric", iGetMaterialIndex ) || angle_zero
 		end
 	end
 
-	if !fGetMuzzleFlashPosition && IsValid( pEntity ) then
-		pOwner = pEntity:GetOwner()
+	if !bCustomPosition && IsValid( pEntity ) then
 		if IsValid( pOwner ) then
 			local iAttachment = pData:GetAttachment()
 			v = self:GetTracerShootPos( vector_origin, pEntity, iAttachment )
@@ -65,7 +67,7 @@ function EFFECT:Init( pData )
 		end
 	end
 
-	if !fGetMuzzleFlashAngles then
+	if !bCustomAngles then
 		fGetAngles = function()
 			if !IsValid( pOwner ) then return angle_zero end
 			return pOwner:EyeAngles()
@@ -78,31 +80,63 @@ function EFFECT:Init( pData )
 	local pEmitter = ParticleEmitter( fGetPos() )
 	local flLifeTime = 1 / pData:GetMagnitude()
 	local flScale = 1 - .75 * math_random() * math_random()
-	local flHeatHazeScale = 1
 
+	local bFirstPerson
 	if pOwner == LocalPlayer() && !pOwner:ShouldDrawLocalPlayer() then
 		flScale = flScale * .5
-	else flHeatHazeScale = 1 / 3 end
+		bFirstPerson = true
+	end
 
 	local aAngles = pData:GetAngles()
 	local vForward = Vector( 1, 0, 0 )
 	local vRight = Vector( 0, -1, 0 )
 	local vUp = Vector( 0, 0, 1 )
 
-	for i = 1, 64 do // No, I am not kidding
+	for i = 1, 32 do
 		local pPart = pEmitter:Add( "effects/muzzleflash" .. math_random( 1, 4 ), fGetPos() )
 
 		local flResultingSpreadRight = ( math_Rand( -.5, .5 ) + math_Rand( -.5, .5 ) )
 		local flResultingSpreadUp = ( math_Rand( -.5, .5 ) + math_Rand( -.5, .5 ) )
 		local vAdd = ( vForward + flResultingSpreadRight * vRight + flResultingSpreadUp * vUp ):GetNormalized()
-		pPart:SetVelocity( vVelocity + vAdd / math_max( .1, abs( flResultingSpreadRight ) + abs( flResultingSpreadUp ) ) * flScale / flLifeTime * 4 )
+		pPart:SetVelocity( vVelocity + vAdd / math_max( .1, abs( flResultingSpreadRight ) + abs( flResultingSpreadUp ) ) * flScale / flLifeTime * 3 )
 		pPart.m_vOffset = Vector()
 
 		pPart:SetDieTime( flLifeTime * math_Rand( .5, 1.5 ) )
 		pPart:SetStartAlpha( 255 )
 		pPart:SetEndAlpha( 0 )
 		pPart:SetStartSize( 0 )
-		pPart:SetEndSize( math_Rand( 16, 20 ) * flScale )
+		pPart:SetEndSize( math_Rand( 12, 16 ) * flScale )
+		pPart:SetRoll( math_Rand( 180, 480 ) )
+		pPart:SetRollDelta( math_Rand( -1, 1 ) )
+		pPart:SetColor( 255, 180, 120 )
+		pPart:SetAirResistance( 140 )
+
+		pPart:SetNextThink( CurTime() )
+		pPart:SetThinkFunction( function()
+			local vOffset = pPart.m_vOffset
+			vOffset:Add( pPart:GetVelocity() * FrameTime() )
+			local v = Vector( vOffset )
+			v:Rotate( fGetAngles() )
+			v:Add( fGetPos() )
+			pPart:SetPos( v )
+			pPart:SetNextThink( CurTime() )
+		end )
+	end
+
+	for i = 1, 32 do
+		local pPart = pEmitter:Add( "effects/muzzleflash" .. math_random( 1, 4 ), fGetPos() )
+
+		local flResultingSpreadRight = ( math_Rand( -.5, .5 ) + math_Rand( -.5, .5 ) ) * .2
+		local flResultingSpreadUp = ( math_Rand( -.5, .5 ) + math_Rand( -.5, .5 ) ) * .2
+		local vAdd = ( vForward + flResultingSpreadRight * vRight + flResultingSpreadUp * vUp ):GetNormalized()
+		pPart:SetVelocity( vVelocity + vAdd / math_max( .1, abs( flResultingSpreadRight ) + abs( flResultingSpreadUp ) ) * flScale / flLifeTime * 3 )
+		pPart.m_vOffset = Vector()
+
+		pPart:SetDieTime( flLifeTime * math_Rand( .5, 1.5 ) )
+		pPart:SetStartAlpha( 255 )
+		pPart:SetEndAlpha( 0 )
+		pPart:SetStartSize( 0 )
+		pPart:SetEndSize( math_Rand( 12, 16 ) * flScale )
 		pPart:SetRoll( math_Rand( 180, 480 ) )
 		pPart:SetRollDelta( math_Rand( -1, 1 ) )
 		pPart:SetColor( 255, 180, 120 )
@@ -121,7 +155,7 @@ function EFFECT:Init( pData )
 	end
 
 	for i = 1, 8 do
-		local pPart = pEmitter:Add( "MuzzleFlashHeatHaze", fGetPos() )
+		local pPart = pEmitter:Add( bFirstPerson && "MuzzleFlashHeatHazeView" || "MuzzleFlashHeatHazeWorld", fGetPos() )
 
 		local flResultingSpreadRight = ( math_Rand( -.5, .5 ) + math_Rand( -.5, .5 ) )
 		local flResultingSpreadUp = ( math_Rand( -.5, .5 ) + math_Rand( -.5, .5 ) )
@@ -129,10 +163,10 @@ function EFFECT:Init( pData )
 		pPart:SetVelocity( vVelocity + vAdd / math_max( .1, abs( flResultingSpreadRight ) + abs( flResultingSpreadUp ) ) * flScale / flLifeTime * 4 )
 		pPart.m_vOffset = Vector()
 
-		pPart:SetDieTime( flLifeTime * math_Rand( 4, 8 ) )
+		pPart:SetDieTime( flLifeTime * math_Rand( 3, 6 ) )
 		pPart:SetStartAlpha( 0 )
 		pPart:SetEndAlpha( 255 )
-		pPart:SetStartSize( math_Rand( 32, 48 ) * flScale * flHeatHazeScale )
+		pPart:SetStartSize( math_Rand( 32, 48 ) * flScale )
 		pPart:SetEndSize( 0 )
 		pPart:SetRoll( math_Rand( 180, 480 ) )
 		pPart:SetRollDelta( math_Rand( -3, 3 ) )
